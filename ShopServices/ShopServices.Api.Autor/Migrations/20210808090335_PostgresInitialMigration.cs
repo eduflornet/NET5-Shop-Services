fc5d0824983a12f8d168e047d8a9ad7a6e933ce2
Eduardo Flores <@eduflornet>
2021-08-08 11:19:54 +0200

    Added basic structure in regards to Api.Author and initial migration with Entity Framework Tools with Postgres Database

---
+++ ShopServices/ShopServices.Api.Autor/Migrations/20210808090335_PostgresInitialMigration.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace ShopServices.Api.Author.Migrations
+{
+    public partial class PostgresInitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuthorBook",
+                columns: table => new
+                {
+                    AuthorBookId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    LastName = table.Column<string>(type: "text", nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    AuthorBookGuid = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorBook", x => x.AuthorBookId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Degree",
+                columns: table => new
+                {
+                    DegreeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    AcademicCenter = table.Column<string>(type: "text", nullable: true),
+                    DegreeDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    AuthorBookId = table.Column<int>(type: "integer", nullable: false),
+                    DegreeGuid = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Degree", x => x.DegreeId);
+                    table.ForeignKey(
+                        name: "FK_Degree_AuthorBook_AuthorBookId",
+                        column: x => x.AuthorBookId,
+                        principalTable: "AuthorBook",
+                        principalColumn: "AuthorBookId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Degree_AuthorBookId",
+                table: "Degree",
+                column: "AuthorBookId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Degree");
+
+            migrationBuilder.DropTable(
+                name: "AuthorBook");
+        }
+    }
+}
---
+++ ShopServices/ShopServices.Api.Autor/Persistence/ContextAuthor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopServices.Api.Author.Model;
+
+namespace ShopServices.Api.Author.Persistence
+{
+    public class ContextAuthor:DbContext
+    {
+        public ContextAuthor(DbContextOptions<ContextAuthor> options) : base(options) { }
+        public DbSet<AuthorBook> AuthorBook { get; set; }
+        public DbSet<Degree> Degree { get; set; }
+    }
+}
