deaaccc78b44c275e4ea94bf75c4e647b3986f3c
Eduardo Flores <@eduflornet>
2021-08-13 18:10:10 +0200

    Created a QueryFilter class to query a workbook by its ID using the EF Core Context SQL Server database In the constructor of this, an instance of AutoMapper and ContextLibrary was injected And a Handler class was created to get the data using EF Core Context and LINQ This QueryFilter class is called from the controller through the Send () method.

---
+++ ShopServices/ShopService.Api.Book/Application/QueryFilter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ShopService.Api.Book.Model;
+using ShopService.Api.Book.Persistence;
+
+namespace ShopService.Api.Book.Application
+{
+    public class QueryFilter
+    {
+        public class UniqueBook : IRequest<BookDto>
+        {
+            public Guid? BookId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<UniqueBook, BookDto>
+        {
+            private readonly ContextLibrary _contextLibrary;
+            private readonly IMapper _mapper;
+            public Handler(ContextLibrary contextLibrary, IMapper mapper)
+            {
+                _contextLibrary = contextLibrary;
+                _mapper = mapper;
+            }
+
+            public async Task<BookDto> Handle(UniqueBook request, CancellationToken cancellationToken)
+            {
+                var book = await _contextLibrary.LibraryMaterial.Where(x => x.LibraryMaterialId == request.BookId).FirstOrDefaultAsync();
+                if(book == null) throw new Exception("The book was not found");
+
+                var bookDto = _mapper.Map<LibraryMaterial, BookDto>(book);
+                return bookDto;
+            }
+        }
+    }
+}
--- ShopServices/ShopService.Api.Book/Controllers/BookController.cs
+++ ShopServices/ShopService.Api.Book/Controllers/BookController.cs
@@ -31,5 +31,11 @@ namespace ShopService.Api.Book.Controllers
             return await _mediator.Send(new Query.BookList());
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BookDto>> GetUniqueBook(Guid id)
+        {
+            return await _mediator.Send(new QueryFilter.UniqueBook {BookId = id});
+        }
+
     }
 }
