450cc6852c7ab0ded80518ddbd604c2b8da7d8af
Eduardo Flores <@eduflornet>
2021-08-20 20:58:34 +0200

    Added new Web API ShopService.Api.ShoppingCart with the basic structure and all the necessary libraries. Added CartSession and ShoppingCartSessionDetail models and CartContext class ready to generate the migration and the creation of the database with EF Core

---
+++ ShopServices/ShopService.Api.ShoppingCart/Model/ShoppingCartSessionDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ShopService.Api.ShoppingCart.Model
+{
+    public class ShoppingCartSessionDetail
+    {
+        public int ShoppingCartSessionDetailId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string SelectedProduct { get; set; }
+        public int CartSessionId { get; set; }
+        public CartSession CartSession { get; set; }
+
+
+    }
+}
---
+++ ShopServices/ShopService.Api.ShoppingCart/Persistence/CartContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopService.Api.ShoppingCart.Model;
+using DbContext = System.Data.Entity.DbContext;
+
+namespace ShopService.Api.ShoppingCart.Persistence
+{
+    public class CartContext:DbContext
+    {
+        public CartContext(DbContextOptions<CartContext> options):base(options) { }
+        public DbSet<CartSession> CartSession { get; set; }
+        public DbSet<ShoppingCartSessionDetail> ShoppingCartSessionDetail { get; set; }
+
+    }
+}
