7f8e37beb691d41d648dace572eb2808f365919f
Eduardo Flores <@eduflornet>
2021-08-21 13:18:28 +0200

    Added business logic to insert a shoppingCartSession and shoppingCartSessionDetail object in relation to the number of products added to the cart Added configuring MediatR in Startup like this: services.AddMediatR(typeof(Insert.Handler).Assembly); Added method Create into ShoppingCartController

---
+++ ShopServices/ShopService.Api.ShoppingCart/Controllers/ShoppingCartController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ShopService.Api.ShoppingCart.Application;
+
+namespace ShopService.Api.ShoppingCart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ShoppingCartController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Insert.Execute data)
+        {
+            return await _mediator.Send(data);
+        }
+    }
+}
--- ShopServices/ShopService.Api.ShoppingCart/Startup.cs
+++ ShopServices/ShopService.Api.ShoppingCart/Startup.cs
@@ -1,16 +1,12 @@
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using ShopService.Api.ShoppingCart.Application;
 using ShopService.Api.ShoppingCart.Persistence;
 
 namespace ShopService.Api.ShoppingCart
@@ -33,6 +29,7 @@ namespace ShopService.Api.ShoppingCart
             {
                 options.UseMySQL(Configuration.GetConnectionString("ConnectionDataBase"));
             });
+            services.AddMediatR(typeof(Insert.Handler).Assembly);
 
             services.AddSwaggerGen(c =>
             {
