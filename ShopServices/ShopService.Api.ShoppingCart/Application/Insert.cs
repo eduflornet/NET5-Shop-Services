87e8edaf47c8dd6f3c7913e19910242e6d395205
Eduardo Flores <@eduflornet>
2021-08-21 12:10:39 +0200

    Added business logic to insert a shoppingCartSession and shoppingCartSessionDetail object in relation to the number of products added to the cart

---
+++ ShopServices/ShopService.Api.ShoppingCart/Application/Insert.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using ShopService.Api.ShoppingCart.Model;
+using ShopService.Api.ShoppingCart.Persistence;
+
+namespace ShopService.Api.ShoppingCart.Application
+{
+    public class Insert
+    {
+        public class Execute : IRequest
+        {
+            public DateTime SessionCreationDate { get; set; }
+            public List<string> ProductList { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Execute>
+        {
+            private readonly CartContext _cartContext;
+
+            public Handler(CartContext context)
+            {
+                _cartContext = context;
+            }
+
+            public async Task<Unit> Handle(Execute request, CancellationToken cancellationToken)
+            {
+                var cartSession = new CartSession
+                {
+                    CreationDate = request.SessionCreationDate
+
+                };
+                _cartContext.Add(cartSession);
+                var value = await _cartContext.SaveChangesAsync();
+                if(value == 0) throw new Exception("An error occurred in the shopping cart insert");
+                int cartSessionId = cartSession.CartSessionId;
+                foreach (var product in request.ProductList)
+                {
+                    var shoppingCartSessionDetail = new ShoppingCartSessionDetail
+                    {
+                        CreationDate = DateTime.Now,
+                        CartSessionId = cartSessionId,
+                        SelectedProduct = product
+                    };
+                    _cartContext.ShoppingCartSessionDetail.Add(shoppingCartSessionDetail);
+
+                }
+
+                value = await _cartContext.SaveChangesAsync();
+                if (value > 0) return Unit.Value;
+                throw new Exception("An error occurred in the insert of the shoppingCartSessionDetail");
+
+            }
+        }
+    }
+}
